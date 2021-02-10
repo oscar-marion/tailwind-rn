7fb7b353d5dde89caf8ed18a6a14af01ce5b98f3
Oscar Marion <oscar.marion@capgemini.com>
2021-02-10 12:31:41 +0100

    custom output path

--- cli.js
+++ cli.js
@@ -8,6 +8,7 @@ const tailwind = require('tailwindcss');
 const build = require('./build');
 const yargs = require('yargs/yargs');
 const {hideBin} = require('yargs/helpers');
+const isValidPath = require('is-valid-path');
 
 const {argv} = yargs(hideBin(process.argv));
 
@@ -22,12 +23,15 @@ const source = `
 `;
 
 function checkArg(arg) {
-	return arg && arg === path.basename(arg);
+	return arg && isValidPath(arg);
 }
 
 postcss([checkArg(argv.i) ? tailwind(argv.i) : tailwind])
 	.process(source, {from: undefined})
 	.then(({css}) => {
+		if (checkArg(argv.o)) {
+			fs.promises.mkdir(path.dirname(argv.o), {recursive: true});
+		}
 		const styles = build(css);
 		fs.writeFileSync(
 			checkArg(argv.o) ? argv.o : 'styles.json',
